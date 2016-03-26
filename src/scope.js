97e1370b30c838a125cc7e2d06be49f72cfca94c
Fangjuan <fangjuanyuyue@gmail.com>
2016-03-26 10:36:56 +0800

    20160326 共写完6个单元测试

--- src/scope.js
+++ src/scope.js
@@ -1,5 +1,31 @@
 // 'use strict';
 
 function Scope(){
-
+	this.$$watchers = [];
 }
+
+function initWatchVal() { }
+
+Scope.prototype.$watch = function(watchFn, listenerFn){
+	var watcher = {
+		watchFn: watchFn,
+		listenerFn: listenerFn,
+		last: initWatchVal
+	};
+	this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function(){
+	var self = this;
+	var newValue, oldValue;
+	_.forEach(this.$$watchers, function(watcher){
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+		if(newValue !== oldValue){
+			watcher.last = newValue;
+			watcher.listenerFn(newValue, 
+				(oldValue === initWatchVal ? newValue: oldValue), 
+				self);
+		}
+	});
+};
