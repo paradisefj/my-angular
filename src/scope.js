544ce8db7ef9a38c3f3388b1106c6422a00e3b7d
fangjuan <fangjuanyuyue@gmail.com>
2016-03-31 15:56:09 +0800

    基于值的脏值检查完成

--- src/scope.js
+++ src/scope.js
@@ -2,17 +2,20 @@
 
 function Scope(){
 	this.$$watchers = [];
+	this.$$lastDirtyWatch = null;
 }
 
 function initWatchVal() { }
 
-Scope.prototype.$watch = function(watchFn, listenerFn){
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq){
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function(){},
+		valueEq: !!valueEq,
 		last: initWatchVal
 	};
 	this.$$watchers.push(watcher);
+	this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$$digestOnce = function(){
@@ -21,12 +24,17 @@ Scope.prototype.$$digestOnce = function(){
 	_.forEach(this.$$watchers, function(watcher){
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if(newValue !== oldValue){
-			watcher.last = newValue;
+		if(!(self.$$areEqual(newValue, oldValue, watcher.valueEq))){
+
+			self.$$lastDirtyWatch = watcher;
+
+			watcher.last = watcher.valueEq ? _.cloneDeep(newValue) : newValue;
 			watcher.listenerFn(newValue, 
 				(oldValue === initWatchVal ? newValue: oldValue), 
 				self);
 			dirty = true;
+		}else if(self.$$lastDirtyWatch === watcher){
+			return false;
 		}
 	});
 	return dirty;
@@ -35,10 +43,21 @@ Scope.prototype.$$digestOnce = function(){
 Scope.prototype.$digest = function(){
 	var tt1 = 10;
 	var dirty;
+	this.$$lastDirtyWatch = null;
 	do {
 		dirty = this.$$digestOnce();
 		if(dirty && !(tt1 --)) {
 			throw '10 digest iterations reached';
 		}
 	} while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq){
+	if(valueEq){
+		return _.isEqual(newValue, oldValue);
+	}else {
+		return newValue === oldValue||
+			(typeof newValue ===  'number'  && typeof oldValue ===  'number'  &&
+			       isNaN(newValue) && isNaN(oldValue));
+	}
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -134,16 +134,92 @@ describe("Scope", function(){
 				function(scope) { return scope.counterA; },
 				function(newValue, oldValue, scope){
 					scope.counterB ++;
-					console.log(scope.counterB);
 				});
 			scope.$watch(
 				function(scope) { return scope.counterB; },
 				function(newValue, oldValue, scope){
 					scope.counterA ++;
-					console.log(scope.counterA);
 				});
 
 			expect(function() { scope.$digest(); }).toThrow();
+
+		});
+
+		it('ends the digest when the last watch is clean', function(){
+			scope.array = _.range(100);
+			var watchExecutions = 0;
+
+			_.times(100, function(i){
+				scope.$watch(
+					function(scope) {
+						watchExecutions ++;
+						return scope.array[i];
+					},
+					function(newValue, oldValue, scope){
+
+					});
+			});
+
+			scope.$digest();
+			expect(watchExecutions).toBe(200);
+
+			scope.array[0] = 420;
+			scope.$digest();
+			expect(watchExecutions).toBe(301);
+		});
+
+		it('does not end digest so that new watches are not run', function(){
+
+			scope.aValue = 'abc';
+			scope.counter = 0;
+
+			scope.$watch(
+				function(scope) { return scope.aValue; },
+				function(newValue, oldValue, scope){
+					scope.$watch(
+						function(scope) { return scope.aValue; },
+						function(newValue, oldValue, scope){ scope.counter ++; });
+				});
+
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+		});
+
+		it("compares based on value if enabled", function(){
+			scope.aValue = [1, 2, 3];
+			scope.counter = 0;
+
+			scope.$watch(
+				function(scope) { return scope.aValue; },
+				function(newValue, oldValue, scope){
+					scope.counter ++;
+				},
+				true);
+
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+
+			scope.aValue.push(4);
+			scope.$digest();
+			expect(scope.counter).toBe(2);
+			
+		});
+
+		it("correct handle the NaNs", function(){
+			scope.number = 0/0;
+			scope.counter = 0;
+			
+			scope.$watch(
+				function(scope) { return scope.number; },
+				function(newValue, oldValue, scope){
+					scope.counter ++;
+				});
+
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+
+			scope.$digest();
+			expect(scope.counter).toBe(1);
 
 		});
 	});
