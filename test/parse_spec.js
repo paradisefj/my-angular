ccf02844b30ed0b942e25a780562a7b947ae81ed
fangjuan <fangjuanyuyue@gmail.com>
2016-04-24 19:19:49 +0800

    parse a floating number

---
+++ test/parse_spec.js
@@ -0,0 +1,26 @@
+/* jshint globalstrict: true */ 
+/* global parse: false */
+'use strict';
+
+describe('parse', function() {
+	it('can parse an integer', function() {
+		var fn = parse('42');
+		expect(fn).toBeDefined();
+		expect(fn()).toBe(42);
+	});
+
+	it('can parse a floating point number', function() {
+		var fn = parse('4.2');
+		expect(fn()).toBe(4.2);
+	});
+
+	it('can parse a floating point number without an integer part', function() {
+		var fn = parse('.42');
+		expect(fn()).toBe(0.42);
+	});
+
+	// it('can parse a number in scientific notation', function() {
+	// 	var fn = parse('42e3');
+	// 	expect(fn()).toBe(42000);
+	// });
+});
