828750b7339066935d6523a71aa216cb5ee106df
fangjuan <fangjuanyuyue@gmail.com>
2016-11-26 17:53:17 +0800

    完成解析字符串和布尔值

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -73,9 +73,31 @@ describe('parse', function() {
 		expect(fn()).toEqual('a\"b');
 	});
 
-	
+	it('will parse a string with unicode escapes', function() {
+		var fn = parse('"\\u00A0"');
+		expect(fn()).toEqual('\u00A0');
+	});
 
+	it('will not parse a string with invalid unicode escapes', function() {
+		expect(function() {
+			parse('"\\u00T0"');
+		}).toThrow();
+	});
 
+	it('will parse null', function() {
+		var fn = parse('null');
+		expect(fn()).toBe(null);
+	});
+
+	it('will parse true', function() {
+		var fn = parse('true');
+		expect(fn()).toBe(true);
+	});
+
+	it('will parse false', function() {
+		var fn = parse('false');
+		expect(fn()).toBe(false);
+	});
 
 
 
